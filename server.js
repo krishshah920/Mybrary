0c26c060ec502e9384a33af51b479da119678d24
KR1$#N@ <krishshah920@gmail.com>
2020-08-09 17:19:56 +0530

    initial authors edit

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
 
 const mongoose = require('mongoose')
 
@@ -26,5 +30,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
+
 
 app.listen(process.env.PORT || 3000)
